c22575ce204ffb56ee70675aee5a2e0d4d5547f7
Elise Kain <elise.kain@gmail.com>
2015-10-24 21:17:21 -0400

    Changed json data into javascript object

---
+++ flashcards.js
@@ -0,0 +1,104 @@
+var flashcards1 = {
+  "category": "General Aviation",
+  "topic": "Phonetic Alphabet",
+  "questions": {
+  "A": "Alpha",
+  "B": "Bravo",
+  "C": "Charlie",
+  "D": "Delta",
+  "E": "Echo",
+  "F": "Foxtrot",
+  "G": "Golf",
+  "H": "Hotel",
+  "I": "India",
+  "J": "Juliett",
+  "K": "Kilo",
+  "L": "Lima",
+  "M": "Mike",
+  "N": "November",
+  "O": "Oscar",
+  "P": "Papa",
+  "Q": "Quebec",
+  "R": "Romeo",
+  "S": "Sierra",
+  "T": "Tango",
+  "U": "Uniform",
+  "V": "Victor",
+  "W": "Whiskey",
+  "X": "X-ray",
+  "Z": "Zulu"
+  }
+}
+
+var flashcards2 = {
+  "category": "Cessna 172S Skyhawk",
+  "topic": "Emergency Procedures Checklists | Memory Items",
+  "questions": {
+    "q1": [
+      "Engine Failure During Takeoff Roll",
+      "Throttle - IDLE.",
+      "Brakes - APPLY."
+    ],
+    "q2": [
+      "Engine Failure Immediately After Takeoff",
+      "Airspeed - 70 KIAS (flaps UP). 65 KIAS (flaps DOWN)."
+    ],
+    "q3": [
+      "Engine Failure During Flight (Restart Procedures)",
+      "Airspeed - 68 KIAS.",
+      "Fuel Shutoff Valve - ON (push full in).",
+      "Fuel Selector Valve - BOTH",
+      "Auxiliary Fuel Pump Switch - ON.",
+      "Mixture - RICH (if restart has not occurred)."
+    ],
+    "q4": [
+      "Fire: During Start on Ground",
+      "Ignition Switch - START, Continue cranking. If engine fails to start...",
+      "Throttle - FULL OPEN.",
+      "Mixture - IDLE CUT OFF.",
+      "Cranking - CONTINUE.",
+      "Fuel Shutoff Valve - OFF (Pull Full Out).",
+      "Auxiliary Fuel Pump Switch - OFF."
+    ],
+    "q5": [
+      "Engine Fire In Flight",
+      "Mixture - IDLE CUT OFF.",
+      "Fuel Shutoff Valve - Pull Out (OFF).",
+      "Auxiliary Fuel Pump Switch - OFF.",
+      "Master Switch - OFF."
+    ],
+    "q6": [
+      "Electrical Fire in Flight",
+      "Master Switch - OFF.",
+      "Vents, Cabin Air, Heat - CLOSED.",
+      "Fire Extinguisher - ACTIVATE."
+    ],
+    "q7": [
+      "Cabin Fire"
+      "Master Switch - OFF.",
+      "Vents/Cabin Air/Heat - CLOSED.",
+      "Fire Extinguisher - ACTIVATE."
+    ],
+    "q8": [
+      "Wing Fire",
+      "Landing/Taxi Light Switches - OFF.",
+      "Navigation Light Switch - OFF.",
+      "Strobe Light Switch - OFF.",
+      "Pitot Heat Switch - OFF."
+    ],
+    "q9": [
+      "Inadvertent Icing Encounter",
+      "Turn pitot heat switch ON.",
+      "Turn back or change altitude.",
+      "Pull cabin heat control full out and open defroster outlets."
+    ],
+    "q10": [
+      "Static Source Blockage",
+      "Static Pressure Alternate Source View - PULL ON."
+    ],
+    "q11": [
+      "Vacuum System Failure",
+      "Vacuum Gage - CHECK."
+    ]
+  }
+}
