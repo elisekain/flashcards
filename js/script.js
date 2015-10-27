7dbafd43cabef3aa52affaba0cdf45f0c923962a
Elise Kain <elise.kain@gmail.com>
2015-10-27 17:00:08 -0400

    Refactor into flashcard and control objects

--- js/script.js
+++ js/script.js
@@ -5,129 +5,138 @@ $(document).ready(function() {
   var questionIndex = 0;
   var lastQuestionIndex = currentQuestions.length - 1;
 
-  var clearPrevCard = function() {
-    // Clear Previous Card Text and Styling
-    $("#back_side").html("");
-    $("#front_side").html("");
-    $("#back_side").css("font-size", "1.5rem");
-  }
+  var flashcard = {
+    // Set Up Question On Flashcard
+    setQuestions: function() {
+      flashcard.updateCardsLeft();
+      flashcard.clearPrevCard();
+      flashcard.resetCardSide();
+      flashcard.addText();
+    },
 
-  var updateCardsLeft = function() {
-    // Update Cards Left in Stack
-    lastQuestionIndex = currentQuestions.length - 1;
-    $(".count").text(currentQuestions.length);
-  }
+    updateCardsLeft: function() {
+      // Update Cards Left in Stack
+      lastQuestionIndex = currentQuestions.length - 1;
+      $(".count").text(currentQuestions.length);
+    },
 
-  var finishMsg = function() {
-    if (lastQuestionIndex === -1) {
-      $("#front_side").append("<p class='finishMsg'>You've completed this flashcard stack!<br> Press 'R' to reset</p>");
-      return true;
-    } else {
-      return false;
-    }
-  }
+    clearPrevCard: function() {
+      // Clear Previous Card Text and Styling
+      $("#back_side").html("");
+      $("#front_side").html("");
+      $("#back_side").css("font-size", "1.5rem");
+    },
 
-  // Hide Back Side of Card; Show Front Side
-  var resetCardSide = function() {
-    $("#back_side").addClass("hide");
-    $("#front_side").removeClass("hide");
-  }
+    // Hide Back Side of Card; Show Front Side
+    resetCardSide: function() {
+      $("#back_side").addClass("hide");
+      $("#front_side").removeClass("hide");
+    },
 
-  // Set Up Question On Flashcard
-  var setQuestions = function() {
-    updateCardsLeft();
-    clearPrevCard();
-    resetCardSide();
+    addText: function() {
+      if (!flashcard.finishMsg()) {
+        // Append text to front and back sides.
+        currentQuestions[questionIndex].forEach(function(e, i) {
+          if (i === 0) {
+            $("#front_side").append("<p>" + currentQuestions[questionIndex][i] + "</p>");
+          } else {
+            $("#back_side").append("<p>" + currentQuestions[questionIndex][i] + "</p>");
+          }
 
-    if (!finishMsg()) {
-      // Append text to front and back sides.
-      currentQuestions[questionIndex].forEach(function(e, i) {
-        if (i === 0) {
-          $("#front_side").append("<p>" + currentQuestions[questionIndex][i] + "</p>");
-        } else {
-          $("#back_side").append("<p>" + currentQuestions[questionIndex][i] + "</p>");
-        }
+          // Reduce font size if more than 2 items on back
+          if (i > 2) {
+            $("#back_side").css("font-size", "1rem");
+          }
+        });
+      }
+    },
 
-        // Reduce font size if more than 2 items on back
-        if (i > 2) {
-          $("#back_side").css("font-size", "1rem");
-        }
-      })
-    }
-  }
-
-  // Remove Question if "Mark Complete" Box is checked
-  var markComplete = function() {
-    if ($("input:checked").length) {
-      currentQuestions.splice(questionIndex, 1);
-      $("#mark_complete").prop("checked", false);
-      return true;
-    }
-  }
-
-  // Go to Next Flashcard
-  var next = function() {
-    if (!markComplete()) {
-      if (questionIndex === lastQuestionIndex) {
-        questionIndex = 0;
+    finishMsg: function() {
+      if (lastQuestionIndex === -1) {
+        $("#front_side").append("<p class='finishMsg'>You've completed this flashcard stack!<br> Press 'R' to reset</p>");
+        return true;
       } else {
-        questionIndex++;
+        return false;
       }
     }
-    setQuestions();
   }
 
-  // Go to Previous Flashcard
-  var prev = function() {
-    markComplete();
-    if (questionIndex === 0) {
-      questionIndex = lastQuestionIndex;
-    } else {
-      questionIndex--;
-    }
-    setQuestions();
-  }
-
-  // Randomize Cards
-  var randomize = function() {
-    var randomNum = Math.floor(Math.random() * currentQuestions.length);
-    return randomNum;
-  }
-
-  // Randomize / Mixup Cards Cards
-  var mixupCards = function() {
-    markComplete();
-    questionIndex = randomize();
-    setQuestions();
-  }
-
-  // Flip Flashcard
-  var flip = function() {
-    $("#back_side").toggleClass("hide");
-    $("#front_side").toggleClass("hide");
-  }
-
-  // Reset
-  var resetFlashcards = function() {
-    currentQuestions = userSelectedQuestions.slice(0);
-    questionIndex = 0;
-    lastQuestionIndex = currentQuestions.length - 1;
-    setQuestions();
-  }
 
   var selectTopic = function(topicName, topBorderColor) {
-    userSelectedQuestions = topicName.questions.slice(0);
+    flashcard.userSelectedQuestions = topicName.questions.slice(0);
     $(".topic").text(topicName.topic);
     $("#flashcard").css("border-top-color", topBorderColor);
-    resetFlashcards();
+    controls.resetFlashcards();
+  }
+
+  var controls = {
+
+    // Flip Flashcard
+    flip: function() {
+      $("#back_side").toggleClass("hide");
+      $("#front_side").toggleClass("hide");
+    },
+
+    // Reset
+    resetFlashcards: function() {
+      currentQuestions = userSelectedQuestions.slice(0);
+      questionIndex = 0;
+      lastQuestionIndex = currentQuestions.length - 1;
+      flashcard.setQuestions();
+    },
+
+    // Randomize Cards
+    randomize: function() {
+      var randomNum = Math.floor(Math.random() * currentQuestions.length);
+      return randomNum;
+    },
+
+    // Randomize / Mixup Cards Cards
+    mixupCards: function() {
+      controls.markComplete();
+      questionIndex = controls.randomize();
+      flashcard.setQuestions();
+    },
+
+    // Go to Previous Flashcard
+    prev: function() {
+      controls.markComplete();
+      if (questionIndex === 0) {
+        questionIndex = lastQuestionIndex;
+      } else {
+        questionIndex--;
+      }
+      flashcard.setQuestions();
+    },
+
+    // Go to Next Flashcard
+    next: function() {
+      if (!controls.markComplete()) {
+        if (questionIndex === lastQuestionIndex) {
+          questionIndex = 0;
+        } else {
+          questionIndex++;
+        }
+      }
+      flashcard.setQuestions();
+    },
+
+    // Remove Question if "Mark Complete" Box is checked
+    markComplete: function() {
+      if ($("input:checked").length) {
+        currentQuestions.splice(questionIndex, 1);
+        $("#mark_complete").prop("checked", false);
+        return true;
+      }
+    }
   }
 
   //Event Listeners
-  $("#flip").on("click", flip);
-  $("#next").on("click", next);
-  $("#prev").on("click", prev);
-  $("#random").on("click", mixupCards);
-  $("#reset").on("click", resetFlashcards);
+  $("#flip").on("click", controls.flip);
+  $("#next").on("click", controls.next);
+  $("#prev").on("click", controls.prev);
+  $("#random").on("click", controls.mixupCards);
+  $("#reset").on("click", controls.resetFlashcards);
 
   //Switch to Topic 1
   $(".topic1").on("click", function() {
@@ -181,24 +190,25 @@ $(document).ready(function() {
   $("body").keyup(function(e) {
     // 70 is "F""
     if (e.which == 70 && e.target == document.body) {
-      flip();
+      controls.flip();
     // 37 is left arrow
     } else if (e.which == 37 && e.target == document.body) {
-      prev();
+      controls.prev();
     // 39 is right arrow
     } else if (e.which == 39 && e.target == document.body) {
-      next();
+      controls.next();
     // 82 is "R"
     } else if (e.which == 82 && e.target == document.body) {
-      resetFlashcards();
+      controls.resetFlashcards();
     // 81 is "Q"
     } else if (e.which == 81 && e.target == document.body) {
-      mixupCards();
+      controls.mixupCards();
     }
   });
 
   //Set Up Flashcards
-  setQuestions();
+  flashcard.setQuestions();
 });
 
 // Create flashcard object to contain functions.
+// Event Listeners only work on document body. Still want to prevent from activating while in inputs...
