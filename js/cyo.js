d0108e8d568d4e9cf05edfd12b7ff85310fb5ba6
Elise Kain <elise.kain@gmail.com>
2016-01-16 17:56:49 -0500

    CYO refactor and add Handlebars

--- js/cyo.js
+++ js/cyo.js
@@ -4,26 +4,33 @@ var cyo = {
   flashcards_cyo: {},
   cardNumber: 1,
 
-  // Get data to create CYO flashcard Stack
+  // Get data to create CYO flashcard stack
   createYourOwn: function() {
     this.flashcards_cyo = {};
     this.flashcards_cyo.topic = $("input[name='cyo_topic']").val();
     this.flashcards_cyo.questions = [];
+
     $(".card_entries").each(function(i) {
-      var thisCardContent = [];
-      if ($(".card_entries .front").eq(i).val() && $(".card_entries .back").eq(i).val()) {
-        thisCardContent.push($(".card_entries .front").eq(i).val());
-        thisCardContent.push($(".card_entries .back").eq(i).val());
-        cyo.flashcards_cyo.questions.push(thisCardContent);
-      }
+      var frontSide = $(".card_entries .front").eq(i).val();
+      var backSide = $(".card_entries .back").eq(i).val();
+      this.addCustomCard(frontSide, backSide);
     })
+  },
+
+  addCustomCard: function(frontSide, backSide) {
+    var cardContent = [];
+    if (frontSide && backSide) {
+      thisCardContent.push(frontSide);
+      thisCardContent.push(backSide);
+      cyo.flashcards_cyo.questions.push(thisCardContent);
+    }
   },
 
   // Add more input fields for CYO
   addMoreCYOCards: function() {
     if (this.cardNumber < 50) {
       for (var i = 0; i < 5; i++) {
-        $(".cyo_cards").append("<div class='card_entries'><label for='front" + this.cardNumber + "'>Card &#35;" + this.cardNumber + "</label><input type='text' class='front' name='front" + this.cardNumber + "' placeholder='Front'><input type='text' class='back' name='back" + this.cardNumber + "' placeholder='Back'></div>");
+        $(".cyo_cards").append(Handlebars.getTemplate("addCards"));
         this.cardNumber++;
       }
     }
