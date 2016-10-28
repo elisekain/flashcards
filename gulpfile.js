1a8aff879c7009c0f710bbb427b47d17678ece62
Elise Kain <elise.kain@gmail.com>
2016-10-27 22:17:59 -0400

    minify css and add sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,28 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var useref = require('gulp-useref');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var cleanCss = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
+
 var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function() {
-    return gulp.src('src/scss/**/*.scss')
+    return gulp.src('src/scss/main.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass())
+        .pipe(cleanCss())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('public/dist/css'));
 });
 
-gulp.task('scripts', function() {
-    return gulp.src('src/js/**/*.js')
-        .pipe(concat('main.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('public/dist/js'));
+gulp.task('useref', function() {
+    return gulp.src('index.html')
+        .pipe(useref())
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(gulp.dest('public/dist'));
 });
 
 gulp.task('browserSync', function() {
@@ -26,7 +35,7 @@ gulp.task('browserSync', function() {
     })
 });
 
-gulp.task('watch', ['browserSync', 'scripts', 'sass'], function() {
+gulp.task('watch', ['browserSync', 'useref', 'sass'], function() {
     gulp.watch('src/scss/**/*.scss', ['sass']);
-    gulp.watch('src/js/**/*.js', ['scripts', browserSync.reload]);
+    gulp.watch('src/js/**/*.js', ['useref', browserSync.reload]);
 });
